1480ade2a6d6c56b50035544bc2a56c08a60612c
eva-gor <evagor82@gmail.com>
2023-05-16 15:58:45 +0300

    functions/this/bind/call/apply

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const x = {x: "x", toString: function(){return "x"}};
-const y = {y : "y", toString: function(){return "kuku"}};
-const d = {x: 10, y: 20};
-// x.toString = function(){return "xx"};
-// d[x] = 100;
-// console.log(d);
-// d[y] = 200;
-// console.log(d);
-// console.log(d[x]);
-// const f = function(){return "xx"};
-const num = 2;
-num.x = 100;
-console.log(num.x);
-const mn = {};
-mn.x = 100;
-console.log(mn, mn.x);
-let ddd;
-// ddd.x = 200; //TypeError because indefined doesn't have wrapping class
-(1+2).xxxx = 1000;
+this.x = 100;
+// console.log(this);
+function f1(){
+    return this;
+}
 
-let str1 = "abcd";
-str1[0] = '*';
-console.log(str1[0]);
+const f2 = () =>{
+    return this;
+};
 
-// f.x = function(a,b){
-//     return a + b;
-// }
-// // console.log(f.x(10, 20));
-// // console.log((2).x); // = num.x
+// // console.log('f1 = ', f1());
+// // console.log('f2 =', f2());
+// // console.log((() =>{
+// //     console.log(this);
+// // })());
+// const x = {f1: function(){
+//     return this;
+// }, f2: ()=> this};
+// console.log('x.f1 calls result ', x.f1());
+// console.log('x.f2 calls result ', x.f2());
 
-// const xx={xx:"45x"};
-// const dd = {xx: 10};
-// // dd['xx']= 20;
-// console.log(dd, xx, dd.xx);
+const rectangle = {width: 20, height: 20, square: function(){
+    return this.width * this.height
+}, perimeter: () => (this.width + this.height)*2};
+console.log(`square = ${rectangle.square()}`);
+console.log(`permiter = ${rectangle.perimeter()}`);
 
-// [2].x = 10;
-// console.log([2].x);
-// const ar =[2];
-// ar.x = 10;
-// console.log(ar, Array.from({length : 2}), ar[x]);
-// console.log(Array.from({length:26}).map((__, index)=>String.fromCharCode(index + 'a'.charCodeAt(0))).map(s => `<div>${s}</div>`).join(''));
+
+const point = {x: 3, y: 4};
+function displayPoint(z, t){
+console.log(`x = ${this.x}, y = ${this.y}, z = ${z}, t = ${t}`);
+}
+
+const d1 = displayPoint.bind(point, 200, 300);
+d1.call();
+
+displayPoint.apply(point, [300, 400]);
+
+
+
+
 
