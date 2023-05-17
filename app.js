6015b82f5a29361ea802b0745b6d56d90aa73660
eva-gor <evagor82@gmail.com>
2023-05-17 11:02:22 +0300

    js-bind + json

--- app.js
+++ app.js
@@ -19,11 +19,15 @@ const f2 = () =>{
 // console.log('x.f1 calls result ', x.f1());
 // console.log('x.f2 calls result ', x.f2());
 
-const rectangle = {width: 20, height: 20, square: function(){
+const rectangle1 = {width: 20, height: 20, square: function(){
+    return this.width * this.height
+}, perimeter: function () {return (this.width + this.height)*2}};
+
+const rectangle2 = {height: 20, width: 20, square: function(){
     return this.width * this.height
 }, perimeter: () => (this.width + this.height)*2};
-console.log(`square = ${rectangle.square()}`);
-console.log(`permiter = ${rectangle.perimeter()}`);
+console.log(`square = ${rectangle1.square()}`);
+console.log(`permiter = ${rectangle1.perimeter()}`);
 
 
 const point = {x: 3, y: 4};
@@ -36,7 +40,11 @@ d1.call();
 
 displayPoint.apply(point, [300, 400]);
 
+console.log(JSON.stringify(rectangle1) == JSON.stringify(rectangle2));
 
-
-
+//to create deep copy object
+const rectangle = JSON.parse(JSON.stringify(rectangle1));
+//on another hand shallow copy:
+const rectangle3 = {...rectangle};
+console.log(s);
 
