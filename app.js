fc34119c0cc07bee0edc38556c71be3667b40b5e
eva-gor <evagor82@gmail.com>
2023-05-18 11:09:40 +0300

    bind -hw

--- app.js
+++ app.js
@@ -1,55 +1,25 @@
-
-Array.prototype.myforEach = function (f) {
-    for (let i = 0; i < this.length; i++) {
-        this[i] = f(this[i], i, this);
+var pokemon = {
+    firstname: 'Pika',
+    lastname: 'Chu ',
+    getPokeName: function () {
+        var fullname = this.firstname + ' ' + this.lastname;
+        return fullname;
     }
 };
-//test
-[1, 2, 3].myforEach(x => console.log(x));
 
-Array.prototype.myfilter = function (f) {
-    let res = [];
-    this.forEach((v, k) => res.push(f(v, k, this)));
-    return res;
+var pokemonName = function (s) {
+    console.log(this.getPokeName() + 'I choose you!' + s);
+};
+
+Function.prototype.myBind = function (obj, ...bindedArgs) {
+    return (...args) => {
+        obj.bindedFunction = this;
+        const res = obj.bindedFunction(bindedArgs, ...args);
+        delete obj.bindedFunction;
+        return res;
+    };
 }
-//test
-console.log([1, 2, 3].myfilter(x => x % 2 == 0));
 
-Array.prototype.mymap = function (f) {
-    for (let i = 0; i < this.length; i++)
-        this[i] = f(this[i], i, this);
-    return this;
-}
-//test
-console.log([1, 2, 3].mymap(x => x ** 3));
+let logPokemon = pokemonName.myBind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+logPokemon(' The end'); // 'Pika Chu I choose you!'
 
-Array.prototype.myReduce = function (f, first) {
-    let acc = first ? f(first, this[0], 0, this) : this[0];
-    for (let i = 1; i < this.length; i++) {
-        acc = f(acc, this[i], i,this);
-    }
-    return acc;
-}
-//test
-console.log([1, 2, 3, 4].myReduce((acc, curr) => { return acc + curr }));
-console.log([1, 2, 3, 4].myReduce((acc, curr) => { return acc + curr }, 1));
-
-////////////
-
-class Deferred {
-    #ar=[];
-    constructor() {
-    }
-    then(f) {
-        this.#ar.push(f);
-    }
-    resolve(s) {this.#ar.forEach(f => s = f(s));
-    }
-}
-const d = new Deferred();
-d.then(function(res){ console.log("1 ", res); return "a"; });
-
-d.then(function(res){ console.log("2 ", res); return "b"; });
-
-d.then(function(res){ console.log("3 ", res); return "c"; });
-d.resolve('hello');
