58d7657efaa3fa53edd5d9766163b9614e2034af
eva-gor <evagor82@gmail.com>
2023-05-23 12:23:38 +0300

    async

--- dictionary.js
+++
@@ -1,7 +0,0 @@
- function readDict(filename, length) {
-    const fs = require("fs");
-    const content = fs.readFileSync(filename).toString();
-    const ar = content.split(/\r?\n/).filter(w => w.length == length);
-    return ar;
-}
-console.log(readDict('./sources/dict.txt', 5));
